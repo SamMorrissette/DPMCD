e5c45e31870b46b9b73c2e05c9c948671128d882
SamMorrissette <samuel.morrissette01@gmail.com>
2024-05-18 17:45:08 -0500

    trycatch debug

--- R/LPML.R
+++ R/LPML.R
@@ -3,16 +3,20 @@ LPML <- function(dp_object) {
   LPML_vec <- rep(NA, num_models)
   for (i in 1:num_models) {
     # This is to ensure proper conversion to armadillo cubes
-    print(dp_object[[i]])
     print(dp_object[[i]]$BMDS_X)
     dim = ncol(dp_object[[i]]$BMDS_X)
     print(dim)
-    if (dim == 1) {
-      dp_object[[i]]$X <- simplify2array(lapply(seq_len(ncol(dp_object[[i]]$X)), 
-             function(t) dp_object[[i]]$X[,t,drop=FALSE]))
-      dp_object[[i]]$means <- simplify2array(lapply(seq_len(ncol(dp_object[[i]]$means)), 
-                                     function(t) t(dp_object[[i]]$means[,t])))
-    }
+    tryCatch({
+      if (dim == 1) {
+        dp_object[[i]]$X <- simplify2array(lapply(seq_len(ncol(dp_object[[i]]$X)), 
+                                                  function(t) dp_object[[i]]$X[,t,drop=FALSE]))
+        dp_object[[i]]$means <- simplify2array(lapply(seq_len(ncol(dp_object[[i]]$means)), 
+                                                      function(t) t(dp_object[[i]]$means[,t])))
+      }
+    }, error = function(cond) {
+      print(dp_object[[i]])
+    })
+
     LPML_vec[i] <- CalcLPML(dp_object[[i]])
   }
   return(LPML_vec)
